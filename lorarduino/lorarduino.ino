d40e2b97a095d9b4cebf4d199f827892368f27a0
Jacques De Sylva <jdesylva@hotmail.com>
2023-06-27 08:39:48 -0400

    Version en développement. Code d'impression des messages inclu (println).

--- lorarduino/lorarduino.ino
+++ lorarduino/lorarduino.ino
@@ -7,7 +7,7 @@
 // Initialiser la constante identifiant le port utilisé pour la remise à zéro du module LoRa RLYR896
 #define RESET 4
 
-// Initialiser le lien série de LoRa sur les ports 2 & 3 
+// Initialiser le lien série de LoRa sur les ports 2 & 3
 SoftwareSerial lora(2, 3);
 
 // initialize the library by associating any needed LCD interface pin
@@ -22,9 +22,11 @@ void lcd_EcrireChaine(char ligne, char* str);
 void RYLR890_WriteString(const char* strBuffer);
 void RYLR890_Sync();
 void RYLR890_Reset();
+const char* RYLR890_Stat(unsigned char);
+void RYLR890_Configure_L1(unsigned char ucSF = 7, unsigned char ucBW = 7, unsigned char ucCR = 1, unsigned char ucPR = 4);
 
 unsigned int quantite_recue = 0;
- 
+
 void setup() {
 
   // Initialiser le port série de débogage à 115200 baud
@@ -48,7 +50,7 @@ void setup() {
   lcd.print("Bienvenue dans");
   //- Positionner le curseur de l’afficheur sur la ligne 2, colonne 1.
   lcd.setCursor(0, 1);
-  //- Écrire la chaîne « cours 243-L36 » sur la seconde ligne.}
+  //- Écrire la chaîne « cours 243-Y55 » sur la seconde ligne.}
   lcd.print("le cours 243-Y55");
   delay(5000);
   // - Effacer l'afficheur
@@ -58,12 +60,20 @@ void setup() {
   // RYLR890_Reset();
   RYLR890_Sync();
 
-  delay(5000);
+  delay(2000);
 
   lcd_EcrireChaine(0, "Recepteur LoRa");
   lcd_EcrireChaine(1, "20230626");
 
-  delay(5000);
+  delay(2000);
+
+  RYLR890_Configure_L1();
+  lcd_EcrireChaine(0, RYLR890_Stat(0));
+  lcd_EcrireChaine(1, RYLR890_Stat(1));
+
+  //delay(5000);
+
+
 
 }
 
@@ -71,7 +81,7 @@ void loop() {
   //
   char strBuffer[100];
 
-  while (1) {
+  while (0) {
     strBuffer[0] = 0;
     RYLR890_ReadString(strBuffer);
     // Si on a reçu des données...
@@ -102,6 +112,10 @@ void lcd_Effacer() {
 void lcd_EcrireChaine(char ligne, const char* str) {
   //- Positionner le curseur de l’afficheur sur la colonne 1.
   lcd.setCursor(0, ligne);
+  // Effacer la ligne
+  lcd.print("                ");
+  // Écrire la chaîne sur l'afficheur
+  lcd.setCursor(0, ligne);
   lcd.print(str);
 
 }
@@ -109,8 +123,9 @@ void lcd_EcrireChaine(char ligne, const char* str) {
 
 void RYLR890_WriteString(const char* strBuffer) {
 
-  lora.print(strBuffer);
-  lora.println('\r');
+  lora.println(strBuffer);
+  //lora.println('\r');
+  Serial.println(strBuffer);
 
 }
 
@@ -126,7 +141,18 @@ char* RYLR890_ReadString(char* strBuffer) {
 
   }
 
-  return strBuffer;
+  // Si la chaine n'est pas vide...
+  if (0 != strBuffer[0]) {
+
+    // Effacer \n
+    *(--ptrStr) = 0;
+    // Effacer \r
+    *(--ptrStr) = 0;
+  }
+  String str = String(strBuffer);
+  str.trim();
+  
+  return str.c_str();
 }
 
 void RYLR890_Sync() {
@@ -134,16 +160,16 @@ void RYLR890_Sync() {
   char tampon[] = "                    ";
   char* reponse = 0;
   int i = 0;
-  
+
   RYLR890_WriteString("AT");
   delay(100);
   lcd_EcrireChaine(0, "Recepteur LoRa");
-  
+
   while (lora.available() == 0) {
     lcd_EcrireChaine(1, String(i++).c_str());
     delay(1000);
   }
-  
+
   if (lora.available() > 0) {
     reponse = RYLR890_ReadString(tampon);
     lcd_EcrireChaine(1, (const char *)reponse);
@@ -161,11 +187,64 @@ void RYLR890_Reset() {
 
 }
 
+const char*  RYLR890_Stat(unsigned char index) {
+
+  char tampon[100];
+  char* reponse = 0;
+
+  if (index == 0) {
+
+    RYLR890_WriteString("AT+PARAMETER?");
+    delay(100);
+    while (lora.available() == 0) {
+      delay(10);
+      Serial.print(".");
+    }
+
+    if (lora.available() > 0) {
+      //      return RYLR890_ReadString(tampon);
+      RYLR890_ReadString(tampon);
+      Serial.println(tampon);
+      tampon[12] = 'P';
+      tampon[13] = 'a';
+      tampon[14] = 'r';
+      reponse = &tampon[12];
+      Serial.println("Tampon[0] == " + String(tampon[0]));
+      Serial.flush();
+      delay(100);
+      Serial.println("Tampon[1] == " + String(tampon[1]));
+      Serial.flush();
+      delay(100);
+      Serial.println("Tampon[2] == " + String(tampon[2]));
+      Serial.flush();
+      delay(100);
+      Serial.println("Tampon[3] == " + String(tampon[3]));
+      Serial.flush();
+      delay(100);
+      Serial.println("Tampon[4] == " + String(tampon[4]));
+      Serial.flush();
+      delay(100);
+      Serial.println("Tampon[5] == " + String(tampon[5]));
+      Serial.println("Tampon[6] == " + String(tampon[6]));
+      Serial.println("Tampon[7] == " + String(tampon[7]));
+      Serial.flush();
+      delay(100);
+      Serial.println(reponse);
+      Serial.flush();
+      return reponse;
+    } else {
+      //
+      return "Erreur";
+    }
+  } else {
+    return "";
+  }
+}
 
 void RYLR890_Configure_L1(unsigned char ucSF = 7, unsigned char ucBW = 7, unsigned char ucCR = 1, unsigned char ucPR = 4) {
 
-  digitalWrite(RESET, 0);
-  delay(500);
-  digitalWrite(RESET, 1);
+  String str = String("AT+PARAMETER=") + String(ucSF) + String(",") + String(ucBW) + String(",") + String(ucCR) + String(",") + String(ucPR); 
+  RYLR890_WriteString(str.c_str());
+  delay(10);
 
 }
